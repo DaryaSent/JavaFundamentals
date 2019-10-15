6a562a346f51adb1afe48cc64765b6d37c3f4127
DaryaSent <54043090+DaryaSent@users.noreply.github.com>
2019-10-15 03:07:15 +0300

    Update LibraryCreator.java

--- task2/src/com/homework/task2/creator/LibraryCreator.java
+++ task2/src/com/homework/task2/creator/LibraryCreator.java
@@ -1,7 +1,7 @@
 package com.homework.task2.creator;
 
+import com.homework.task2.factory.BooksFactory;
 import com.homework.task2.library.Book;
-import com.homework.task2.validator.ListValidator;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
@@ -13,16 +13,16 @@ public class LibraryCreator {
     public static List<Book> createLibrary(List<String> listFromFile) {
         List<Book> library = new ArrayList<>();
 
-        logger.debug("Creating Book List from lines of valid List");
+        logger.debug("----Creating Book List from lines of valid List----");
         for (String lines : listFromFile) {
             logger.debug("Split line");
             String[] splitLines = lines.split("; +");
-            logger.debug("Creating book from line:  ");
-            Book book = new BooksCreator().createBooks(splitLines);
+            logger.debug("Creating book from line:::  ");
+            Book book = new BooksFactory().createBooks(splitLines);
             library.add(book);
         }
 
-        logger.info("Library of Book List was created successfully");
+        logger.info("----Library of Book List was created successfully----");
         return library;
     }
 }
